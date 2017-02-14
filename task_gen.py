266d113844064b6c8cb06ec408310d25cccddb10
Martin <ryqiem@gmail.com>
2017-02-14 09:47:44 +0100

    Added assertions and tasks

--- task_gen.py
+++ task_gen.py
@@ -22,7 +22,7 @@ fw.writerow(['TYPE', 'CONTENT', 'PRIORITY', 'INDENT', 'AUTHOR', 'RESPONSIBLE', '
 ###############
 
 
-def todo_w(content, priority="4", indent="0", delta=None, date=None):
+def todo_w(content, priority="4", indent="0", delta=None, date=None, d_pages=None, n_pages=None):
     """
         Todoist-formatted row writer
 
@@ -38,7 +38,10 @@ def todo_w(content, priority="4", indent="0", delta=None, date=None):
 
     assert date != None
 
-    fw.writerow(['task', content, priority, indent, '', '', date, 'dk'])
+    if time != None:
+        fw.writerow(['task', content, priority, indent, '', '', date, 'dk', time])
+    else:
+        fw.writerow(['task', content, priority, indent, '', '', date, 'dk'])
 
 ####################
 ## RUN THE SCRIPT ##
@@ -49,17 +52,28 @@ for row in fr:
     if fr.line_num == 1:  # Skip first row
         continue
 
-    category, subject, chapter, date = row
+    """
+        n_pages: Amount of pages
+        d_pages: The interval to study
+    """
+
+    category, subject, date, d_pages, n_pages = row
 
     # Check for empty fields
     assert category != ""
     assert subject != ""
 
-    if category == "F":  # Run this if lecture
-        todo_w("Tjekke om slides til {subject} er kommet op".format(subject=subject), priority="1", delta=-1, date=date)
-        todo_w("Gennemgå forelæsningsslides til {subject}".format(subject=subject), priority="1", delta=1, date=date)
+    if category == "K":
+        assert n_pages != ""
+        assert d_pages != ""
 
+    # Create the tasks
+    if category == "F":  # Run this if lecture
+        time = 5
+        todo_w("Gennemgå noter til forelæsning om {subject} og lav flashcards af det (5) @fokus".format(subject=subject), priority=2, delta=1, date=date)
+        todo_w("Gennemgå forelæsningsslides til {subject} (5) @fokus".format(subject=subject), priority=3, delta=2, date=date)
     elif category == "H":  # Run this if class
         pass
     elif category == "K":  # Run this if chapter
-        pass
+        time = int(n_pages) * 5
+        todo_w("Læse og lave løbende flashcards til {subject} (pp. {d_pages}) ({time}) @fokus".format(subject=subject, d_pages=d_pages, time=time), priority=1, delta=-1, date=date)
